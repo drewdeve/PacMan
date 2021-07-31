3eeea9b12c3b613278e2fa12ac216c53997dbc3f
Andrey Proskurin <prosn1ke11@gmail.com>
2021-08-01 00:11:51 +0300

    version 1.3

--- Form1.cs
+++ Form1.cs
@@ -12,21 +12,30 @@ namespace PacMan
 {
     public partial class Form1 : Form
     {
+        private int score = 0;
+        private int heroStep = 2;
+        private int enemyStep = 1;
+        private int heroHorVelocity = 0;
+        private int heroVerVelocity = 0;
 
-        private int step = 2; 
-        private int horVelocity = 0;
-        private int verVelocity = 0;
+        private int enemyHorVelocity = 0;
+        private int enemyVerVelocity = 0;
+        private string enemyDirection = "down";
+
         private string heroDirection = "right";
         private int heroImageCount = 1;
         private int foodImageCount = 1;
+        private int enemyImageCount = 1;
+
+        private bool heroIsPredator = false;
 
         Random rand = new Random();
 
         public Form1()
         {
             InitializeComponent();
-            timer1.Start();
             timerHero.Start();
+            timerEnemy.Start();
 
             this.BackColor = Color.Blue;
             Hero.BackColor = Color.Transparent;
@@ -34,6 +43,7 @@ namespace PacMan
 
             Food.BackColor = Color.Transparent;
             Food.SizeMode = PictureBoxSizeMode.StretchImage;
+            Food.Image = (Image)Properties.Resources.ResourceManager.GetObject("food_1");
 
             Enemy.BackColor = Color.Transparent;
             Enemy.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -43,37 +53,92 @@ namespace PacMan
         {
             if(e.KeyCode == Keys.A)
             {
-                horVelocity = -step;
-                verVelocity = 0;
+                heroHorVelocity = -heroStep;
+                heroVerVelocity = 0;
                 heroDirection = "left";
             }
             else if(e.KeyCode == Keys.D)
             {
-                horVelocity = step;
-                verVelocity = 0;
+                heroHorVelocity = heroStep;
+                heroVerVelocity = 0;
                 heroDirection = "right";
             }
             else if(e.KeyCode == Keys.W)
             {
-                horVelocity = 0;
-                verVelocity = -step;
+                heroHorVelocity = 0;
+                heroVerVelocity = -heroStep;
                 heroDirection = "up";
             }
             else if(e.KeyCode == Keys.S)
             {
-                horVelocity = 0;
-                verVelocity = step;
+                heroHorVelocity = 0;
+                heroVerVelocity = heroStep;
                 heroDirection = "down";
             }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Hero.Left += horVelocity;
-            Hero.Top += verVelocity;
+            MoveHero();
+            MoveEnemy();
+            EnemyBorderCollision();
             HeroBorderCollision();
             HeroEnemyCollision();
             HeroFoodCollision();
+            HeroWallCollision();
+        }
+
+        private void MoveHero()
+        {
+            Hero.Left += heroHorVelocity;
+            Hero.Top += heroVerVelocity;
+        }
+
+        private void MoveEnemy()
+        {
+            Enemy.Left += enemyHorVelocity;
+            Enemy.Top += enemyVerVelocity;
+
+            if(enemyDirection == "right")
+            {
+                enemyHorVelocity = enemyStep;
+                enemyVerVelocity = 0;
+            }
+            else if(enemyDirection == "left")
+            {
+                enemyHorVelocity = -enemyStep;
+                enemyVerVelocity = 0;
+            }
+            else if (enemyDirection == "down")
+            {
+                enemyHorVelocity = enemyStep;
+                enemyVerVelocity = 0;
+            }
+            else if (enemyDirection == "up")
+            {
+                enemyHorVelocity = -enemyStep;
+                enemyVerVelocity = 0;
+            }
+        }
+
+        private void EnemyBorderCollision()
+        {
+            if(Enemy.Left + Enemy.Width < 0)
+            {
+                Enemy.Left = ClientRectangle.Width;
+            }
+            if(Enemy.Left > ClientRectangle.Width)
+            {
+                Enemy.Left = 0 - Enemy.Width;
+            }
+            if(Enemy.Top + Enemy.Height < 0)
+            {
+                Enemy.Top = ClientRectangle.Height;
+            }
+            if(Enemy.Top > ClientRectangle.Height)
+            {
+                Enemy.Top = 0 - Enemy.Height;
+            }
         }
 
         private void HeroFoodCollision()
@@ -85,11 +150,23 @@ namespace PacMan
                 Food.Top = rand.Next(0, 401);
                 foodName = "food_" + foodImageCount;
                 Food.Image = (Image)Properties.Resources.ResourceManager.GetObject(foodName);
+
+                if(foodImageCount == 4)
+                {
+                    heroIsPredator = true;
+                }
+                else
+                {
+                    heroIsPredator = false;
+                }
+
                 foodImageCount++;
                 if(foodImageCount > 4)
                 {
                     foodImageCount = 1;
                 }
+
+                UpdateScore(100);
             }
         }
 
@@ -117,9 +194,28 @@ namespace PacMan
         {
             if (Hero.Bounds.IntersectsWith(Enemy.Bounds))
             {
-                timer1.Stop();
-                MessageBox.Show("Game Over!");
+                if (heroIsPredator)
+                {
+                    Enemy.Dispose();
+                    UpdateScore(500);
+                }
+                else
+                {
+                    GameOver(); 
+                }
             }
+        }
+
+        private void GameOver()
+        {
+            timer1.Stop();
+            MessageBox.Show("Game Over!");
+        }
+
+        private void UpdateScore(int scoreChange)
+        {
+            score += scoreChange;
+            ScoreLabel.Text = "Score: " + score;
         }
 
         private void ButtonStart_Click(object sender, EventArgs e)
@@ -128,8 +224,17 @@ namespace PacMan
             Enemy.Visible = true;
             Food.Visible = true;
 
+            foreach(Control c in Controls)
+            {
+                if((string)c.Tag == "wall")
+                {
+                    c.Visible = true;
+                }
+            }
+
             ButtonStart.Visible = false;
             this.Focus();
+            timer1.Start();
         }
 
         private void timerHero_Tick(object sender, EventArgs e)
@@ -144,6 +249,48 @@ namespace PacMan
             }
         }
 
+        private void HeroWallCollision()
+        {
+            foreach(Control c in Controls)
+            {
+                if((string)c.Tag == "wall")
+                {
+                    if (c.Bounds.IntersectsWith(Hero.Bounds))
+                    {
+                        heroHorVelocity = 0;
+                        heroVerVelocity = 0;
+                    }
+                } 
+            }
+        }
 
+        private void timerEnemy_Tick(object sender, EventArgs e)
+        {
+            string imageName;
+            imageName = "enemy_" + enemyDirection + "_" + enemyImageCount;
+            Enemy.Image = (Image)Properties.Resources.ResourceManager.GetObject(imageName);
+            enemyImageCount += 1;
+            if(enemyImageCount > 2)
+            {
+                enemyImageCount = 1;
+            }
+
+            if(Hero.Left - Enemy.Left > Hero.Top - Enemy.Top && Hero.Left - Enemy.Left > 0)
+            {
+                enemyDirection = "right";
+            }
+            else if (Hero.Left - Enemy.Left < Hero.Top - Enemy.Top && Hero.Left - Enemy.Left < 0)
+            {
+                enemyDirection = "left";
+            }
+            else if(Hero.Top - Enemy.Top > 0)
+            {
+                enemyDirection = "down";
+            }
+            else if(Hero.Top - Enemy.Top < 0)
+            {
+                enemyDirection = "up";
+            }
+        }
     }
 }
